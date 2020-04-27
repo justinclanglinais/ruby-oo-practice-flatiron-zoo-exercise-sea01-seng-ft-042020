6cc5987ee8cf10b797c4965bf49587565ad4cf03
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-26 18:23:38 -0700

    commit

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,18 @@
 class Animal
-
+    attr_accessor :zoo,:weight
+    attr_reader :nickname,:species
+    @@all=[]
+    def initialize(nickname,species,weight,zoo)
+        @nickname = nickname
+        @species = species
+        @weight = weight.to_i
+        @zoo = zoo
+        @@all << self
+    end
+    def self.find_by_species(species)
+        self.all.select{|animal|animal.species==species}
+    end
+    def self.all
+        @@all
+    end
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,19 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+woodland = Zoo.new("Woodland Park","Seattle, WA")
+bubba = Zoo.new("Big Bubba","Seattle, WA")
+wynewood = Zoo.new("Wynewood Exotic Animal Park","Wynewood, OK")
+baskin = Zoo.new("Big Cat Rescue","Somewhere, FL")
 
+koko = Animal.new("Koko","gorilla","295",woodland)
+kamau = Animal.new("Kamau","gorilla","228",woodland)
+lucy = Animal.new("Lucy","dog","21",woodland)
+harambe = Animal.new("Harambe","gorilla","345",bubba)
+liz = Animal.new("Liz","lizard","2",wynewood)
+rob = Animal.new("Robert","lizard","3",woodland)
+gerald = Animal.new("Geraldine","giraffe","1900",baskin)
+marvin = Animal.new("Marvin","monkey","36",wynewood)
 
 binding.pry
 puts "done"
